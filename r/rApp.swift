ae3bf4070bf73a9590981466fb34a87699cd9b82
Rahaf <Rahaf.98.h.d@gmail.com>
2024-09-25 11:38:04 +0300

    Initial Commit

---
+++ r/rApp.swift
@@ -0,0 +1,17 @@
+//
+//  rApp.swift
+//  r
+//
+//  Created by Rahaf ALDossari on 22/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct rApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ rTests/rTests.swift
@@ -0,0 +1,17 @@
+//
+//  rTests.swift
+//  rTests
+//
+//  Created by Rahaf ALDossari on 22/03/1446 AH.
+//
+
+import Testing
+@testable import r
+
+struct rTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
